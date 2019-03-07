de9532e660a36bc4ce634d15ac22ce6196a9a9e1
Deschant <kounoudeschant@gmail.com>
2019-03-07 23:33:05 +0100

    [Feature #164172932] Add endpoint for fetching unread messages

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -8,7 +8,7 @@ export default (method) => {
     case 'checkToken': {
       return [
         // eslint-disable-next-line consistent-return
-        header('token', 'Token is missing or invalid').custom(async (value, { req }) => {
+        header('token', 'Token is missing or expired').custom(async (value, { req }) => {
           try {
             const result = await token.verifyToken(value);
             if (result) {
@@ -59,7 +59,7 @@ export default (method) => {
     case 'newMessage': {
       return [
         // eslint-disable-next-line consistent-return
-        header('token', 'Token is invalid').custom(async (value, { req }) => {
+        header('token', 'Token is missing or expired').custom(async (value, { req }) => {
           try {
             const result = await token.verifyToken(value);
             if (result) {
